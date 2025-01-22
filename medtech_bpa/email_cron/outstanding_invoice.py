a2865247a3ab256f7c68960047bef406b79b4360
priyatoshindictrans <priyatosh.s@indictranstech.com>
2025-01-22 13:51:25 +0530

    outstanding invoice amount

--- medtech_bpa/email_cron/outstanding_invoice.py
+++ medtech_bpa/email_cron/outstanding_invoice.py
@@ -1,11 +1,13 @@
 import frappe
-from frappe.utils import nowdate, add_days, formatdate
+from frappe.utils import nowdate, add_days, formatdate,getdate
 
 @frappe.whitelist()
 def send_invoice_summary_email():
     # Calculate the target date (30 days before today)
-    target_date = add_days(nowdate(), -30)
     
+    target_date = add_days(nowdate(), -30)
+ 
+    print("target_date", target_date,"\n\n\n")
     # Fetch invoices using Frappe ORM
     invoices = frappe.get_all(
         "Sales Invoice",
@@ -16,7 +18,7 @@ def send_invoice_summary_email():
         },
         fields=["name", "customer", "outstanding_amount", "posting_date"]
     )
-    
+    #print("invoices\n\n\n",invoices)
     # Group invoices by customer
     grouped_invoices = {}
     for invoice in invoices:
@@ -31,6 +33,7 @@ def send_invoice_summary_email():
     # Send separate email for each customer
     for customer, invoices in grouped_invoices.items():
         # Prepare email content
+        
         email_content = f"<h3>Outstanding Invoices for {customer} (More Than 30 Days)</h3>"
         email_content += "<table border='1' style='width:100%; border-collapse: collapse;'>"
         email_content += "<tr><th>Invoice No</th><th>Invoice Date</th><th>Outstanding Amount</th></tr>"
@@ -38,24 +41,15 @@ def send_invoice_summary_email():
             email_content += f"<tr><td>{invoice['name']}</td><td>{formatdate(invoice['posting_date'])}</td><td>{invoice['outstanding_amount']}</td></tr>"
         email_content += "</table><br>"
         
+       # print("\n\n ",email_content,"\n\n")
         # Email subject
         subject = f"Outstanding More Than 30 Days for Customer: {customer}"
         
         # Send email for this customer
-        frappe.sendmail(
-            recipients=recipients,
-            subject=subject,
-            message=email_content
-        )
-
-# Schedule the task
-def daily_invoice_summary_scheduler():
-    send_invoice_summary_email()
-
-# Hook this function to a daily scheduler in hooks.py
-# hooks.py
-# scheduler_events = {
-#     "daily": [
-#         "medtech_bpa.email_cron.outstanding_invoice.send_invoice_summary_email"
-#     ]
-# }
+        # frappe.sendmail(
+        #     recipients=recipients,
+        #     subject=subject,
+        #     message=email_content
+        # )
+        #return {}
+        
