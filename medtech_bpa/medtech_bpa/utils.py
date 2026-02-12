a66a72c4f3fb29c4f3638f7f5c3c7cd9b8187d20
jayatipardeshi <jayati.r@indictranstech.com>
2026-02-12 10:29:44 +0530

    Implement daily payment reminder and auto-disable customer after credit period via scheduler

--- medtech_bpa/medtech_bpa/utils.py
+++ medtech_bpa/medtech_bpa/utils.py
@@ -2,7 +2,7 @@ import frappe
 from frappe.utils import now_datetime, time_diff_in_hours,pretty_date, now, add_to_date
 from datetime import datetime, date
 from email.utils import formataddr
-
+from frappe.utils import nowdate, getdate
 def delete_email_queues():
     # '''method deletes email queues whose status is in Sent and/or Error'''
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! weekday ---", frappe.utils.get_datetime().weekday() )
@@ -38,5 +38,49 @@ def delete_email_queues():
             %(deletable_email_queues)s;
         ''', values={'deletable_email_queues':deletable_email_queues_tuple})
 
-        
+def daily_credit_check():
+
+    today = getdate(nowdate())
+
+    invoices = frappe.get_all(
+        "Sales Invoice",
+        filters={
+            "docstatus": 1,
+            "outstanding_amount": (">", 0)
+        },
+        fields=["name", "customer", "due_date"]
+    )
+
+    customer_overdue = {}
+
+    for inv in invoices:
+
+        customer = frappe.get_doc("Customer", inv.customer)
+
+        if not customer.custom_reminder_emails:
+            continue
+
+        recipients = [
+            e.strip()
+            for e in customer.custom_reminder_emails.replace("\n", ",").split(",")
+            if e.strip()
+        ]
+
+        # send reminder from due date
+        if today >= getdate(inv.due_date):
+            frappe.sendmail(
+                recipients=recipients,
+                subject="Payment Reminder",
+                message=f"Invoice {inv.name} payment pending. Kindly clear dues."
+            )
+
+        # mark overdue
+        if today > getdate(inv.due_date):
+            customer_overdue[customer.name] = 1
+
+    # disable customers having overdue invoices
+    for cust in customer_overdue:
+        frappe.db.set_value("Customer", cust, "disabled", 1)
+
+    frappe.db.commit()        
             
